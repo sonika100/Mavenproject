f131e50f8bed66193fea858250d00b07aca42bed
sonika <sonikaasharma05@gmail.com>
2020-05-07 14:22:26 +0530

    Done

---
+++ src/test/java/LoginScript.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginScript {
+	WebDriver driver;
+	@Test
+	public void launchBrowser()
+	{
+		String driverpath=System.getProperty("user.dir")+"\\src\\test\\java\\BrowserDriver\\chromedriver.exe";
+		System.out.println(driverpath);
+		System.setProperty("webdriver.chrome.driver",driverpath);
+	    driver= new ChromeDriver();
+	}
+
+}
